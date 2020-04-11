4a4ea87cbedb890b0d6b839987d0a62f99b66c25
Pabluwu <lopez.pablo2305@gmail.com>
2020-04-11 13:56:26 -0400

    models+views

--- SVT/portal/models.py
+++ SVT/portal/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
+class posibleCliente(models.Model):
+    idPosibleCliente = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=30)
+    telefono = models.IntegerField(validators=[MaxValueValidator(999999999)])
+    email = models.CharField(max_length=30)
+    mensaje = models.TextField(max_length=350)
+    copiaMensaje = models.BooleanField(null=True)
+
+    def guardar(self):
+        self.save() 
+
+    def __str__(self):
+        return self.nombre
--- SVT/portal/views.py
+++ SVT/portal/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
+from django.core.mail import EmailMultiAlternatives
+from .models import posibleCliente
+from django.conf import settings
+from django.template.loader import render_to_string
+
 
 # Create your views here.
+def index(request):
+    # send_mail('Hello from mi casa', 'Hello this is an automatic message', 'olml12354@gmail.com', ['lopez.pablo2305@gmail.com'], fail_silently=False)
+
+    if request.method == "POST":
+
+        if request.POST.get('copia', False) == False:
+            sendCopy = False
+        else:
+            sendCopy = True
+        correo = posibleCliente.objects.create(nombre=request.POST['nombre'],
+                                                telefono = request.POST['fono'],
+                                                email = request.POST['mail'],
+                                                mensaje=request.POST['mensaje'],
+                                                copiaMensaje=sendCopy)
+        
+        context = {
+            'user' : correo.nombre,
+            'email' : correo.email, 
+            'message' : correo.mensaje,
+            'phone' : correo.telefono   
+        }
+        # contact_message = get_template('portal/contact_message.txt').render(context)
+        subject = "El usuario {0} quiere contactarse contigo.".format(correo.nombre)
+        to_email = ['palito2305@gmail.com']
+
+        text_content = render_to_string('portal/contact_message.txt', context, request=request)
+        html_content = render_to_string('portal/plantillamensaje.html', context, request=request)
+
+        
+        if correo.copiaMensaje == False:
+            emailMessage = EmailMultiAlternatives(subject=subject, body=text_content, from_email=settings.EMAIL_HOST_USER, to=[to_email])
+            emailMessage.attach_alternative(html_content, "text/html")
+            emailMessage.send(fail_silently=True)
+        else:
+            emailMessage = EmailMultiAlternatives(subject=subject, body=text_content, from_email=settings.EMAIL_HOST_USER, cc=[correo.email], to=[to_email])
+            emailMessage.attach_alternative(html_content, "text/html")
+            emailMessage.send(fail_silently=True)
+            
+        
+        
+        # from_email = settings.EMAIL_HOST_USER
+        
+        # send_mail(subject, contact_message, from_email, to_email, fail_silently=True )
+        return render(request, "portal/index.html", {})
+    else:
+        print('ADIOS mundo')
+        return render(request, "portal/index.html", {}) 
+    return render(request, "portal/index.html", {})
